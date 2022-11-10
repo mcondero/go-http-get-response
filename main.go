c7561b3ea3e152d7f38cc18bfd54e72bc5bc53ee
Maurício Conde Ramon <92527061+mcondero@users.noreply.github.com>
2022-11-10 00:03:49 -0300

    "feat: adicionando comentários"

--- main.go
+++ main.go
@@ -15,20 +15,25 @@ func main() {
 		"http://amazon.com",
 	}
 
+	// ! configurando um canal de comunicação entre a função main e as demais go routines
 	c := make(chan string)
 
+	// ! para cada link dentro do range de links, rodar uma função checkLink
 	for _, link := range links {
 		go checkLink(link, c)
 	}
 
+	// ! para cada endereço de link, realiza uma função anônima que cadastra o link em outro endereço de memória, evitando loop
 	for l := range c {
 		go func(link string) {
+			// ! tempo em espera na função anônima para evitar um bloqueio de firewall
 			time.Sleep(5 * time.Second)
 			checkLink(link, c)
 		}(l)
 	}
 }
 
+// ! função de checagem de http response GET, sinalizando conclusão por um canal para a função principal
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
